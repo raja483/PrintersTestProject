6da4d0866c132dd037608d95ed1de3d7d43b2ffa
Rajasekhar <j.raja483@gmail.com>
2025-04-10 18:56:31 +0530

    Initial Commit

---
+++ RSPrinter/RSPrinterApp.swift
@@ -0,0 +1,32 @@
+//
+//  RSPrinterApp.swift
+//  RSPrinter
+//
+//  Created by Rajasekhar on 10/04/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RSPrinterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
